79d2402b3ec713383ce346cee0ed219574e53b60
S4ntini <santinieduardo92@gmail.com>
2021-02-19 21:12:58 -0300

    minor

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    //console.log(message.content); -> get user input log on cmd
-    //if (!message.author.bot || message.content.startsWith(prefix)) return;
+
+    if (!message.content.startsWith(prefix) || message.author.bot)
+        return;
+    const args = message.content.slice(prefix.lenght).trim().split(' ');
+    const command = args.shift().toLowerCase();
 
     if (message.content === `${prefix}help`) {
         message.channel.send('--Welcome to  FastBuyer--');
         message.channel.send('Commands:');
         message.channel.send('!server -> server specifications');
+        message.channel.send('!userinfo -> uesr information');
     }
     else if (message.content === `${prefix}server`) {
         message.channel.send(`This server's name is: ${message.guild.name}\n` +
@@ -21,15 +25,18 @@ client.on('message', message => {
                               `Created at: ${message.guild.createdAt}\n`+
                               `Based at: ${message.guild.region}`);
     }
-    else if (message.content === `${prefix}user-info`) {
+    else if (message.content === `${prefix}userinfo`) {
         message.channel.send(`Your username: ${message.author.username}\n` +
             `Your ID: ${message.author.id}`);
     }
-    else if (message.content === `${prefix}nike`) {
-        message.channel.send(`Your username: ${message.author.username}\n` +
-                `Your ID: ${message.author.id}`);
+    else if (message.content === `${prefix}test`) {
+        message.channel.send(`args: ${args}\n` +
+            `command: ${command}`);
     }
 
 });
 
 client.login(token);
+
+
+//console.log(message.content); -> get user input log on cmd
