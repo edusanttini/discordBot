b39f24e3fc0fdcdf69318eb4d1710da1fe4bf83b
S4ntini <santinieduardo92@gmail.com>
2021-02-21 13:27:47 -0300

    fix prefix

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot)
         return;
     const args = message.content.slice(prefix.lenght).trim().split(' ');
-    const command = args.shift().toLowerCase();
+    const commandWithPrefix = args.shift().toLowerCase();
+    command = commandWithPrefix.substring(1);
 
-    if (message.content === `${prefix}help`) {
+
+    if (command === 'help') {
         message.channel.send('--Welcome to  FastBuyer--');
         message.channel.send('Commands:');
         message.channel.send('!server -> server specifications');
         message.channel.send('!userinfo -> uesr information');
     }
-    else if (message.content === `${prefix}server`) {
+    else if (command === 'server') {
         message.channel.send(`This server's name is: ${message.guild.name}\n` +
                               `Total Members: ${message.guild.memberCount}\n` +
                               `Created at: ${message.guild.createdAt}\n`+
                               `Based at: ${message.guild.region}`);
     }
-    else if (message.content === `${prefix}userinfo`) {
+    else if (command === 'userinfo') {
         message.channel.send(`Your username: ${message.author.username}\n` +
             `Your ID: ${message.author.id}`);
     }
-    else if (message.content === `${prefix}test`) {
+    else if (command === 'test') {
         message.channel.send(`args: ${args}\n` +
             `command: ${command}`);
     }
